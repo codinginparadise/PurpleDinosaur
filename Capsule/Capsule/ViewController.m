a55814f5d93417e7ac75f946bc2a4b13895ccd0a
Marion Ano <meaola@gmail.com>
2014-06-06 18:30:29 -1000

    Worked on adding initial view to storyboard

---
+++ Capsule/Capsule/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Capsule
+//
+//  Created by Marion Ano on 6/6/14.
+//  Copyright (c) 2014 Marion Ano. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
